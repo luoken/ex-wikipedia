c6b4aedf47075d8adab1dd809a1b9ea7e838fe95
Ken Luo <ken@vody.com>
2021-12-09 14:13:24 -0800

    updates mix.exs, adds license

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule ExWikipedia.MixProject do
       app: :ex_wikipedia,
       version: "0.1.0",
       elixir: "~> 1.11.4",
+      package: package(),
+      description: "Elixir wrapper for Wikipedia's API.",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
@@ -19,27 +21,32 @@ defmodule ExWikipedia.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger, :httpoison]
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
       {:httpoison, "~> 1.8", optional: true},
       {:jason, "~> 1.2", optional: true},
-      {:floki, "~> 0.31.0"},
-      {:mox, "~> 1.0.0"},
+      {:floki, "~> 0.31.0", only: [:dev, :test], optional: true},
+      {:mox, "~> 1.0.0", only: [:test]},
       {:excoveralls, "~> 0.14.3", only: :test},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Ken Luo"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/luoken/ex-wikipedia"}
     ]
   end
 end
